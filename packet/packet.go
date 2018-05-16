805a113007bb87bf00b52d9537c95090b5e24d16
Johannes Brüderl <johannes.bruederl@gmail.com>
2018-05-16 08:47:11 +0200

    move generic packet stuff to packet.go

--- packet/packet.go
+++ packet/packet.go
@@ -81,6 +81,40 @@ func getFixedHeader(r io.Reader) (fh FixedHeader, err error) {
 	return
 }
 
+type ConnectPayload struct {
+	ClientID string
+}
+
+func readConnectPayload(r io.Reader, len int) (ConnectPayload, error) {
+	payloadBytes := make([]byte, len)
+	n, err := io.ReadFull(r, payloadBytes)
+	// TODO set upper limit for payload
+	// TODO only stream it
+	if err != nil {
+		return ConnectPayload{}, err
+	}
+	if n != len {
+		return ConnectPayload{}, errors.New("Payload length incorrect")
+	}
+
+	// CONNECT MUST have the client id
+	// REGEX 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+	// MAY allow more than that, but this must be possible
+
+	// Client Identifier, Will Topic, Will Message, User Name, Password
+
+	// TODO am besten so viel einlesen wie moeglich, und dann reslicen / reader zusammenstecken
+
+	clientIDLengthBytes := payloadBytes[:2]
+	clientIDLength := binary.BigEndian.Uint16(clientIDLengthBytes)
+
+	clientID := string(payloadBytes[2 : 2+clientIDLength])
+	return ConnectPayload{
+		ClientID: clientID,
+	}, nil
+
+}
+
 // Return specific error, so server can answer with correct packet & error code (i.e. CONNACK with error 0x01)
 func ReadPacket(r io.Reader) (ControlPacket, error) {
 	fh, err := getFixedHeader(r)
@@ -100,34 +134,15 @@ func ReadPacket(r io.Reader) (ControlPacket, error) {
 		}
 		payloadLength := fh.RemainingLength - variableHeaderSize
 
-		payloadBytes := make([]byte, payloadLength)
-		n, err := io.ReadFull(r, payloadBytes)
-		// TODO set upper limit for payload
-		// TODO only stream it
+		cp, err := readConnectPayload(r, payloadLength)
 		if err != nil {
 			return nil, err
 		}
-		if n != payloadLength {
-			return nil, errors.New("Payload length incorrect")
-		}
-
-		// CONNECT MUST have the client id
-		// REGEX 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
-		// MAY allow more than that, but this must be possible
-
-		// Client Identifier, Will Topic, Will Message, User Name, Password
-
-		// TODO am besten so viel einlesen wie moeglich, und dann reslicen / reader zusammenstecken
-
-		clientIDLengthBytes := payloadBytes[:2]
-		clientIDLength := binary.BigEndian.Uint16(clientIDLengthBytes)
-
-		clientID := string(payloadBytes[2 : 2+clientIDLength])
 
 		packet := &ConnectControlPacket{
-			FixedHeader:      fh,
-			VariableHeader:   vh,
-			ClientIdentifier: clientID,
+			FixedHeader:    fh,
+			VariableHeader: vh,
+			ConnectPayload: cp,
 		}
 
 		return packet, nil
@@ -140,61 +155,6 @@ func ReadPacket(r io.Reader) (ControlPacket, error) {
 }
 
 // starts with variable header
-func getConnectVariableHeader(r io.Reader) (hdr VariableHeaderConnect, len int, err error) {
-
-	// Protocol name
-	protocolName, n, err := getProtocolName(r)
-	len += n
-	if err != nil {
-		return hdr, 0, err
-	}
-	hdr.ProtocolName = protocolName
-
-	if hdr.ProtocolName != "MQTT" {
-		return hdr, 0, fmt.Errorf("Invalid protocol: %v", hdr.ProtocolName)
-	}
-
-	// Get Proto level
-	protocolLevelBytes := make([]byte, 1)
-	n, err = r.Read(protocolLevelBytes)
-	len += n
-	if err != nil {
-		return
-	}
-	hdr.ProtocolLevel = protocolLevelBytes[0]
-
-	// Get Flags
-	connectFlagsByte := make([]byte, 1)
-	n, err = r.Read(connectFlagsByte)
-	if n != 1 {
-		return hdr, len, errors.New("Failed to read flags byte")
-	}
-	len += n
-	if err != nil {
-		return
-	}
-
-	hdr.ConnectFlags.UserName = connectFlagsByte[0]&128 == 1
-	hdr.ConnectFlags.Passwore = connectFlagsByte[0]&64 == 1
-	hdr.ConnectFlags.WillRetain = connectFlagsByte[0]&32 == 1
-	hdr.ConnectFlags.WillFlag = connectFlagsByte[0]&4 == 1
-	hdr.ConnectFlags.CleanSession = connectFlagsByte[0]&2 == 1
-
-	keepAliveByte := make([]byte, 2)
-	n, err = r.Read(keepAliveByte)
-	len += n
-	if err != nil {
-		return hdr, len, errors.New("Could not read keepalive byte")
-	}
-	if n != 2 {
-		return hdr, len, errors.New("Could not read enough keepalive bytes")
-	}
-
-	hdr.KeepAlive = int(binary.BigEndian.Uint16(keepAliveByte))
-	// TODO Will QoS
-
-	return
-}
 
 // http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html#_Toc398718023
 func getRemainingLength(r io.Reader) (remaining int, err error) {

80383e781c5d5501d03c95147644608193fe0933
Johannes Brüderl <johannes.bruederl@gmail.com>
2018-05-21 00:51:44 +0200

    example: remove dead code

--- cmd/example_server/main.go
+++ cmd/example_server/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
-	"io/ioutil"
 	"net"
 
 	"github.com/infinimesh/mqtt-go/packet"
@@ -12,36 +9,13 @@ import (
 
 //openssl req  -nodes -new -x509  -keyout server.key -out server.cert
 func main() {
-	pair, err := tls.LoadX509KeyPair("./server.cert", "./server.key")
-	if err != nil {
-		panic(err)
-	}
-
-	clientCAPool := x509.NewCertPool()
-
-	crt, _ := ioutil.ReadFile("./server.cert")
-
-	ok := clientCAPool.AppendCertsFromPEM(crt)
-	if !ok {
-		fmt.Println("failed to append cert from pem")
-	}
-
-	cfg := &tls.Config{
-		ClientAuth:   tls.RequireAndVerifyClientCert,
-		Certificates: []tls.Certificate{pair},
-		ClientCAs:    clientCAPool,
-	}
-	_, _ = tls.Listen("tcp", "localhost:8081", cfg)
 	listener, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("waiting for conn")
-
 	for {
 		conn, _ := listener.Accept()
-		fmt.Println("Accept")
 		go handleConn(conn)
 	}
 }

2adf69b8f3dccfaba7051de6a04e2bf77ef7f51e
Preeti Maurya <pritimmaurya@gmail.com>
2020-08-25 15:42:16 +0200

    Revert "test properties length"
    This reverts commit 32ec2c8215d54ef518cc73bd29d248651c8551d4.

--- packet/connect.go
+++ packet/connect.go
@@ -130,22 +130,7 @@ func getConnectVariableHeader(r io.Reader) (hdr ConnectVariableHeader, len int, 
 	} else {
 		hdr.ConnectFlags.WillQoS = 1
 	}
-
-	//reading variable header properties length
-	propertiesLength := make([]byte, 1)
-	n, err = io.ReadFull(r, propertiesLength)
-	if err != nil {
-		return hdr, len, errors.New("Could not read properties length")
-	}
-	fmt.Printf("optional properties length %v and propertiesLength= %v\n ", n, propertiesLength)
-	if n != 1 {
-		fmt.Printf("No optional properties added")
-	} else {
-		hdr.ConnectProperties.PropertyLength = int(binary.BigEndian.Uint16(propertiesLength))
-		len += hdr.ConnectProperties.PropertyLength
-	}
 	fmt.Printf("Connect calculated length n %v\n", len)
-
 	return
 }
 
